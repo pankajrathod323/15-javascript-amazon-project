888d4c39846b30f3a1e94a4adfbab74a90c91119
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-08 01:22:31 +0530

    update calculateDeliveryDate for weekends

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,31 +1,53 @@
-import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
 export const deliveryOptions = [{
-  id:'1',
-  deliveryDays:7,
-  priceCents:0
-},{
-  id:'2',
+  id: '1',
+  deliveryDays: 7,
+  priceCents: 0
+}, {
+  id: '2',
   deliveryDays: 3,
-  priceCents: 499,
-},{
-  id:'3',
+  priceCents: 499
+}, {
+  id: '3',
   deliveryDays: 1,
   priceCents: 999
 }];
 
-export function getDeliveryOption(deliveryOptionId){
-      let deliveryOption;
-     deliveryOptions.forEach((option) =>{
-        if (option.id === deliveryOptionId){
-           deliveryOption = option;
-        }
-     });
-   return deliveryOption || deliveryOptions[0];
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption;
 }
 
-export function calculateDeliveryDate(deliveryOption){
-      const today = dayjs();
-      const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-      const dateString = deliveryDate.format('dddd, MMMM, D');
-     return dateString;
+function isWeekend(date) {
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, 'day');
+
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+      // This is a shortcut for:
+      // remainingDays = remainingDays - 1;
+    }
+  }
+
+  const dateString = deliveryDate.format(
+    'dddd, MMMM D'
+  );
+
+  return dateString;
 }
